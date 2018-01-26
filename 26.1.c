1eb47aab043e1524b0dd161edaca314dc6238c7c
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-01-26 10:03:06 +0530

    Create 26.1.c

---
+++ 26.1.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the number a=");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("%d is even",a);
+    else
+    printf("%d is odd",a);
+    getch();
+}
