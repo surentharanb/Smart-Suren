ea0bf8a8129d287c059357cab8b815dd6ad88aae
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-02-22 10:48:04 +0000

    Create intervel bw prime2.c

---
+++ intervel bw prime2.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+void main()
+{
+int a,b,i,c;
+printf("enter two numbers(intervals):");
+scanf("%d%d",&a,&b);
+printf("prime numbers between %d and %d are :",a , b);
+while(a<b)
+{
+c=0;
+for(i=2;i<=a/2;++i)
+{
+if(a%i==0)
+{
+c=1;
+break;
+}
+}
+if(c==0)
+printf("%d",a);
+++a;
+}
+}
