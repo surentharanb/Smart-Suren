1970d7c0918bb6b19a9a70ed55fecb039e46f37a
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-02-24 11:23:59 +0530

    Create IBM Tasks **.c

---
+++ IBM Tasks **.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+void main()
+{
+    int i,j;
+    clrscr();
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==1||j==1||i==5||j==5||i==j||i==2j||j==2i)
+            {
+                printf("*");
+                printf("\t");
+            }
+            else
+            printf("\t");
+        }printf("\n");
+    }
+}
