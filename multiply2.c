4f1d1d7c90f41a54eb877fd0183c49c7ede59343
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-02-27 10:44:36 +0000

    Create multiply2.c

---
+++ multiply2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+	void main()
+	{
+	    int a,i;
+	    printf("Enter the number:");
+	    scanf("%d",&a);
+	    for(i=1;i<=a;i++)
+	    {
+	        printf("%d\n",a*i);
+	    }
+	}
